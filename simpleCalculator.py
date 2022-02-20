76227dacb824c55ad53dab3246d590fa343e029d
Kenneth <k.l.k94@hotmail.com>
2022-02-20 16:56:10 +0100

    clean up

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -2,11 +2,13 @@
 rawAnswear = input("Enter two numbers separated by space:")
 operation = input("Enter operation(*,/,+,-)")
 rawAnswearSplit = rawAnswear.split()
+
 a = rawAnswearSplit[0]
 b = rawAnswearSplit[1]
+
 a = int(a)
 b = int(b)
-print(a)
+
 if operation == "*":
   c = lambda a, b: a * b
 elif operation == "/":
@@ -15,4 +17,5 @@ elif operation == "+":
   c = lambda a, b: a + b
 elif operation == "-":
   c = lambda a, b: a - b
+
 print(c(a,b))
